07609c94ecafea248d07ec2973216625d397503b
Westender <javaxwestender@gmail.com>
2023-07-11 18:52:59 +0300

    hw2

--- src/Variables.java
+++ src/Variables.java
@@ -1,14 +1,6 @@
 public class Variables {
     public static void main(String[] args) {
-        task1();
+
     }
 
-    public static void task1() {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-    }
 }
